bc5ba93be437be289a204c20e5c7e910e9830627
calwinsteve <calwin.steve@codingmart.com>
2019-12-22 00:40:02 +0530

    content-4, content-5, content-6 with javascript functions

---
+++ script.js
@@ -0,0 +1,14 @@
+function moveright(){
+  document.getElementById("main-content-3-element-2-subelement-2-2").style.transform=" translateX(-640px)";
+}
+function moveleft(){
+    document.getElementById("main-content-3-element-2-subelement-2-2").style.transform=" translateX(40px)";
+}
+var x=0;
+function left(){
+    document.getElementById("scroll").style.transform="translateX(40px)";
+}
+function right(){
+    document.getElementById("scroll").style.transform="translateX(-85vw)";
+
+}
